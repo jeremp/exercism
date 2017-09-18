a36050d0055a637ae4044dc2f3127c971b9c5693
PASSERON Jeremy <jeremy.passeron@docapost.fr>
2017-09-18 10:27:57 +0200

    lets start kindergarten

---
+++ kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jpasseron
+ */
+public class KindergartenGarden {
+
+	public KindergartenGarden(String plants) {
+	}
+
+	KindergartenGarden(String plants, String[] studentArray) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	Object getPlantsOfStudent(String student) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
