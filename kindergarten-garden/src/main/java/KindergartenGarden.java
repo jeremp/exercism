a384a2d378f31d3d9485f1c2ced205516ad948b1
PASSERON Jeremy <jpasseron@gmail.com>
2017-09-18 11:05:14 +0200

    kindergarten done

--- kindergarten-garden/src/main/java/KindergartenGarden.java
+++ kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,8 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 
 /**
  *
@@ -10,15 +12,39 @@
  */
 public class KindergartenGarden {
 
+	private static final String SPLIT_STR = "\n";
+	private String ALPHABET = "ABCDEFGHIJKLMNOPQRSTWXYZ";
+	private String firstRow ;
+	private String secondRow ;
+	
 	public KindergartenGarden(String plants) {
+		this(plants, new String[0]);
 	}
 
-	KindergartenGarden(String plants, String[] studentArray) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	public KindergartenGarden(String plants, String[] studentArray) {		
+		String[] rows = plants.split(SPLIT_STR);
+		if(studentArray.length>0){
+			ALPHABET = Arrays.asList(studentArray).stream().sorted().map((t) -> {
+				return t.substring(0, 1);
+			}).collect(Collectors.joining());
+		}
+		firstRow = rows[0];
+		secondRow = rows[1];
 	}
 
-	Object getPlantsOfStudent(String student) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	public List<Plant> getPlantsOfStudent(String student) {
+		List<Plant> res = new ArrayList<>();
+		int index = getStudentTurn(student);		
+		res.add(Plant.getPlant(firstRow.substring(index, index+1).charAt(0)));
+		res.add(Plant.getPlant(firstRow.substring(index+1, index+2).charAt(0)));
+		res.add(Plant.getPlant(secondRow.substring(index, index+1).charAt(0)));
+		res.add(Plant.getPlant(secondRow.substring(index+1, index+2).charAt(0)));
+		return res;
+	}
+	
+	private int getStudentTurn(String student){
+		String initiale = student.substring(0, 1);
+		return ALPHABET.indexOf(initiale.toUpperCase())*2;
 	}
 	
 }
